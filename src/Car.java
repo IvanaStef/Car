0cb1683b043adbed2414e4148108e52bfaf805e6
Ivana <ivana.stefanovic@gmail.com>
2018-05-29 20:24:30 +0200

    Passengers

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 
 public class Car {
-    
+
     int maxSpeed = 120;
     int minSpeed = 10;
     double weigh = 1000;
@@ -9,10 +9,17 @@ public class Car {
     char condition = 'A';
     double currentPrice = 3000;
     int mileagePassed = 50000;
-    
-    public Car(int maxSpeed, int minSpeed, double weight, boolean isTheCarOn, 
+
+    String model;
+    int maxFuel;
+    int currentFuel;
+    int consumption;
+    int currentPassengers;
+    int maxPassengers;
+
+    public Car(int maxSpeed, int minSpeed, double weight, boolean isTheCarOn,
             String license, char condition, double currentPrice, int mileagePassed) {
-        
+
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.weigh = weight;
@@ -21,9 +28,93 @@ public class Car {
         this.condition = condition;
         this.currentPrice = currentPrice;
         this.mileagePassed = mileagePassed;
-    
+
     }
 
-    Car() { }
-        
+    Car() {
+        this.model = "default";
+        this.currentFuel = 0;
+        this.maxFuel = 100;
+        this.currentPassengers = 1;
+        this.maxPassengers = 5;
+    }
+
+    public void printMainAttributes() {
+
+        System.out.println("Model: " + this.model);
+        System.out.println("Kapacitet rezervoara: " + this.maxFuel);
+        System.out.println("Trenutno gorivo: " + this.currentFuel);
+        System.out.println("Predjena kilometraza " + this.mileagePassed);
+
+        System.out.println("Trenutno putnika: " + this.currentPassengers + " , maksimalno: " + this.maxPassengers);
+        System.out.println();
+
+    }
+
+    public void printAttributes() {
+
+        System.out.println("Maksimalna brzina je: " + this.maxSpeed);
+        System.out.println("Minimalna brzina je: " + this.minSpeed);
+        System.out.println("Registracioni broj je: " + this.license);
+        System.out.println("Trenutna cena: " + this.currentPrice);
+        System.out.println("Predjena kilometraza " + this.mileagePassed);
+    }
+
+    public void changeModel(String customModel) {
+        this.model = customModel;
+
+    }
+
+    public void ChangeMaxFuel(int customMaxFuel) {
+        this.maxFuel = customMaxFuel;
+    }
+
+    public void fuelUp() {
+        this.currentFuel = this.maxFuel;
+    }
+
+    public void changeConsumption(int customConsumption) {
+        this.consumption = customConsumption;
+
+    }
+
+    public void travel(int distance) {
+
+        if (true) {
+
+            this.mileagePassed = this.mileagePassed + distance;
+            this.currentFuel = this.currentFuel - distance * this.consumption;
+        } else {
+            System.out.println("Nema dovoljno goriva");
+        }
+    }
+
+    public void getIn() {
+
+        if (this.currentPassengers  < this.maxPassengers)  {
+            this.currentPassengers = this.currentPassengers + 1;
+            System.out.println("Usao jedan putnik, trenutno putnika:"  + this.currentPassengers);
+
+        } else {
+           System.out.println("Nema vise mesta");  
+            
+
+        }
+
+    }
+
+    public void getOut() {
+
+        if (this.currentPassengers  > 0) {
+            this.currentPassengers = this.currentPassengers - 1;
+            System.out.println("Izasao jedan putnik, trenutno putnika: " + this.currentPassengers);
+
+        } else {
+            System.out.println("Nema vise putnika");
+
+        }
+
+    }
+    
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,52 @@ public class Main {
 
     
     public static void main(String[] args) {
+        
         Car myCar = new Car();
-        System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
+        myCar.printAttributes();
+       
+        
         
         
         Car companyCar = new Car(150, 30, 2000, false, "EFG-456", 'B', 3500, 60000);
-        System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
-    }
+        companyCar.changeModel("Fiat");
+        companyCar.ChangeMaxFuel(150);
+        companyCar.fuelUp();
+        companyCar.changeConsumption(10);
+        
+        companyCar.printMainAttributes();
+        companyCar.travel(10);
+        companyCar.printMainAttributes();
+        
+        Car audi = new Car();
+        audi.changeModel("Audi");
+        audi.fuelUp();
+        audi.changeConsumption(20);
+        
+        audi.printMainAttributes();
+        audi.travel(3);
+        audi.printMainAttributes();
+        
+        audi.getIn();
+        audi.printMainAttributes();
+        audi.getIn();
+        audi.printMainAttributes();
+        audi.getIn();
+        audi.printMainAttributes();
+        audi.getIn();
+        audi.printMainAttributes();
+        audi.getOut();
+        audi.printMainAttributes();
+        audi.getOut();
+        audi.printMainAttributes();
+        audi.getOut();
+        audi.printMainAttributes();
+        audi.getOut();
+        audi.printMainAttributes();
+        
+                
+        
+  }
     
     
     
